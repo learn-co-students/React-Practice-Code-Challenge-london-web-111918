72ff7acde36d38e65bc832b59ec929e922dd4459
joaquimbarreto <joaquim@barreto.co>
2019-02-04 23:27:36 +0000

    once I get the id working all should be fine

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,20 +1,55 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
-// Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+	state = {
+		sushis: [],
+		sushi1: 0,
+		sushi2: 4,
+		money: 100
+	};
 
-  render() {
-    return (
-      <div className="app">
-        <SushiContainer  />
-        <Table />
-      </div>
-    );
-  }
+	componentDidMount = () => {
+		fetch(API)
+			.then(response => response.json())
+			.then(data => {
+				this.setState({
+					sushis: data.map(obj => ({ ...obj, bought: false }))
+				});
+			});
+	};
+
+	showSushis = () => {
+		return this.state.sushis.slice(this.state.sushi1, this.state.sushi2);
+	};
+
+	moreSushis = () => {
+		this.setState({
+			...this.state,
+			sushi1: this.state.sushi1 + 4,
+			sushi2: this.state.sushi2 + 4
+		});
+	};
+
+	boughtSushi = event => {
+		console.log(event.target.id);
+	};
+
+	render() {
+		return (
+			<div className="app">
+				<SushiContainer
+					sushis={this.showSushis()}
+					moreSushis={this.moreSushis}
+					buySushi={this.boughtSushi}
+				/>
+				<Table plates={this.showSushis} money={this.state.money} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,26 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
-      </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
-}
+const Sushi = props => {
+	return (
+		<Fragment>
+			<div className="sushi">
+				<div className="plate">
+					{props.sushi.bought ? null : (
+						<img
+							src={props.sushi.img_url}
+							id={props.sushi.id}
+							alt={props.sushi.name}
+							onClick={props.buy}
+							width="100%"
+						/>
+					)}
+				</div>
+				<h4 className="sushi-details">
+					{props.sushi.name} - ${props.sushi.price}
+				</h4>
+			</div>
+		</Fragment>
+	);
+};
 
-export default Sushi
+export default Sushi;
