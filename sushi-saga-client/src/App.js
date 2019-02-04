26450ff9c1667ef96ac1602662343d7917b5e77d
github username <github email address>
2019-02-04 16:33:52 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -2,16 +2,44 @@ import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
-// Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    position: [0, 4],
+    emptyPlates: [],
+    budget: 50
+  }
+
+  componentDidMount() {
+    return fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({sushis: data})
+    })
+  }
+
+  nextPosition = () => {
+    const newPosition = [this.state.position[0] + 4, this.state.position[1] + 4]
+    this.setState({position: newPosition})
+  }
+
+  addPlate = (price) => {
+    const newArray = this.state.emptyPlates.concat([0])
+    const newBudget = this.state.budget - price
+    this.setState({
+      emptyPlates: newArray,
+      budget: newBudget
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer budget={this.state.budget} sushis={this.state.sushis} position={this.state.position} nextPosition={this.nextPosition} addPlate={this.addPlate}/>
+        <Table emptyPlates={this.state.emptyPlates} budget={this.state.budget}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,15 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
+const SushiContainer = ({sushis, position, nextPosition, addPlate, budget}) => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
+        {sushis.map(sushi =>
+          <Sushi key={sushi.id} budget={budget} sushi={sushi} addPlate={addPlate}/>).slice(position[0], position[1]) 
         }
-        <MoreButton />
+        <MoreButton nextPosition={nextPosition}/>
       </div>
     </Fragment>
   )
